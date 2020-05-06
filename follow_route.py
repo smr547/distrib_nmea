619f0e78fd889a4fe85bf910d4011a0b95957cb3
Steven Ring <smr@southsky.com.au>
2020-05-06 16:44:38 +1000

    Close an endless route, compute headings for each leg

--- follow_route.py
+++ follow_route.py
@@ -3,6 +3,7 @@
 import geojson
 from shapely.geometry import Point
 from geopy.distance import great_circle
+import math
 
 class Leg(object):
     def __init__(self, start_point, end_point):
@@ -17,24 +18,47 @@ class Leg(object):
 
         return great_circle(asLatLongTuple(self.start_point), asLatLongTuple(self.end_point)).miles
 
+    def heading_Deg(self):
+
+        dLon = math.radians(self.end_point.x) - math.radians(self.start_point.x);
+        y = math.sin(dLon) * math.cos(math.radians(self.end_point.y));
+        x = math.cos(math.radians(self.start_point.y))*math.sin(math.radians(self.end_point.y)) - math.sin(math.radians(self.start_point.y))*math.cos(math.radians(self.end_point.y))*math.cos(dLon);
+        hdg = math.degrees(math.atan2(y, x))
+        if hdg < 0: hdg += 360.0
+        return hdg
+
+    def __str__(self):
+        return "Leg of %f NM heading %f degrees" % (self.length_NM(), self.heading_Deg())
+
 
 class Route(object):
     def __init__(self, waypoints, endless=False):
+
+        self.legs = []
 
         if len(waypoints) < 3:
             raise ValueError("Not enough waypoints for Route")
         
         # assemble the legs into a route
         self.first_leg = Leg(waypoints[0], waypoints[1])
+        self.legs.append(self.first_leg)
         prior_leg = self.first_leg 
         for i in range(2, len(waypoints)):
             leg = Leg(prior_leg.end_point, waypoints[i])
+            self.legs.append(leg)
             prior_leg.next = leg
             prior_leg = leg
         self.last_leg = prior_leg
         if endless:
-            prior_leg.next = self.first_leg
+            implied_leg = Leg(self.last_leg.end_point, self.first_leg.start_point)
+            prior_leg.next = implied_leg
+            implied_leg.next = self.first_leg
+            self.legs.append(implied_leg)
+            self.last_leg = implied_leg
         self.endless = endless
+
+    def leg_count(self):
+        return len(self.legs)
 
     def length_NM(self):
         result = 0.0
@@ -64,5 +88,10 @@ if __name__ == "__main__":
 
     # assemble the legs into a route
     route = Route(waypoints, endless=True)
-    print "Route is %f NM" % (route.length_NM())
+    print "Route is %d legs totalling %f NM" % (route.leg_count(), route.length_NM())
+
     
+    # print leg details
+
+    for leg in route.legs:
+        print str(leg)
