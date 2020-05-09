357a9fbeb9792188e0db40b7b196cbc7ff576a98
Steven Ring <smr@southsky.com.au>
2020-05-09 10:42:53 +1000

    Predict position and print NMEA sentences

--- follow_route.py
+++ follow_route.py
@@ -1,13 +1,21 @@
 #!/usr/bin/env python
 
+# Test program. Simulate a vessel's passage around a specifed route 
+# issue NMEA sentences representing position reports and vessel identification details 
+# at specified periods
+
 import geojson
 from shapely.geometry import Point
 from geopy.distance import great_circle
 import math
-from datetime import datetime
+from datetime import datetime as dt
+from copy import deepcopy
+import pygc
+from time import sleep
+import aislib
 
 nm_to_metres = 1852.0
-proximity_radius = 50.0 / nm_to_metres
+# proximity_radius = 50.0 / nm_to_metres
 
 def heading_Deg(from_point, to_point):
     # return the heading in degrees from_point to_point
@@ -27,6 +35,11 @@ def asLatLongTuple(point):
 def distance_NM(from_point, to_point):
     # return the distance in nautical miles from_point to_point
     return great_circle(asLatLongTuple(from_point), asLatLongTuple(to_point)).miles
+
+def new_point_given_distance_and_bearing(from_point, distance_NM, bearing_Deg):
+    result = pygc.great_circle(distance=distance_NM * nm_to_metres, azimuth=bearing_Deg, \
+        latitude=from_point.y, longitude=from_point.x)
+    return Point(result['longitude'], result['latitude'])
 
 
 class Leg(object):
@@ -86,10 +99,29 @@ class Route(object):
         return result
 
 class Vessel(object):
-    def __init__(self, mmsi, name, cruise_speed=7.0):
+    def __init__(self, mmsi, name, cruise_speed=7.0, draught=2.3, callsign='', imo=0):
         self.mmsi = mmsi
         self.name = name
+        self.draught = draught
+        self.imo = imo
+        self.callsign = callsign
         self.cruise_speed = cruise_speed
+
+    def as_AIS_report(self, route={}):
+        # Message Type 5
+        aismsg = aislib.AISStaticAndVoyageReportMessage(mmsi=self.mmsi,
+            imo=self.imo,
+            callsign=self.callsign,
+            shipname=self.name,
+            shiptype=36,
+            to_bow=5,to_stern=5,to_port=1,to_starboard=1, 
+            draught=int(self.draught * 10.0),
+            epfd=1, month=5, day=14, hour=20, minute=15,
+            destination='CANBERRA')
+
+        ais = aislib.AIS(aismsg)
+        payload = ais.build_payload(False)
+        return payload
 
     def __str__(self):
         s = "Vessel %s (%s)" % \
@@ -98,7 +130,7 @@ class Vessel(object):
 
 class LKP(object):
     # Last know position object
-    def __init__(self, vessel, as_at=datetime.utcnow(), leg=None, pos=None, course=0.0, speed=0.0):
+    def __init__(self, vessel, as_at=dt.utcnow(), leg=None, pos=None, course=0.0, speed=0.0):
         self.vessel = vessel
         self.as_at = as_at
         self.leg = leg
@@ -111,16 +143,56 @@ class LKP(object):
             (self.vessel, self.as_at, self.pos, self.course, self.speed)
         return s
 
-    def new(as_at=datetime.utcnow()):
+    def next(self, as_at=dt.utcnow()):
         # Calculation the movement of the vessel along the route and return a new last know position
 
         # heading to next waypoint
         hdg_deg = heading_Deg(self.pos, self.leg.end_point)
+
+        now = dt.utcnow()
+        period_seconds = (now - self.as_at).seconds
+        dist_travelled_NM = self.speed * period_seconds / 3600.0
         
+        # move position along route until we reach distance_travelled
+        pos = deepcopy(self.pos)
+        leg = self.leg
+        hdg = 0.0
+        dist2go = dist_travelled_NM
+        while dist2go > 0.0 and leg is not None:
+            dd = distance_NM(pos, leg.end_point)
+            if dd >= dist2go:
+                hdg = heading_Deg(pos, leg.end_point)
+                # compute new pos (d2dist towards end point of this leg)
+                pos = new_point_given_distance_and_bearing(pos, dist2go, hdg)
+                dist2go = 0.0 # and end
+            else:
+                # move to next waypoint and continue moving
+                dist2go -= dd
+                pos = deepcopy(leg.end_point)
+                leg = leg.next
+
+        return LKP(self.vessel, as_at=now, leg=leg, pos=pos, course=hdg, speed=self.speed)
+
+    def as_AIS_pos_report(self):
+        # return an NMEA encoded position report
         
-        
-        
-        
+        aismsg = aislib.AISPositionReportMessage( \
+            mmsi = self.vessel.mmsi, \
+            status = 8, \
+            sog = int(self.speed*10.0), \
+            pa = 1, \
+            lon = int(self.pos.x * 600000),
+            lat = int(self.pos.y * 600000),
+            # lon = (25*60+00)*10000,
+            # lat = (35*60+30)*10000,
+            cog = int(self.course * 10.0),
+            ts = 40,
+            raim = 1,
+            comm_state = 82419
+        )
+        ais = aislib.AIS(aismsg)
+        payload = ais.build_payload(False)
+        return payload
 
 
 if __name__ == "__main__":
@@ -139,15 +211,28 @@ if __name__ == "__main__":
 
     # assemble the legs into a route
     route = Route(waypoints, endless=True)
-    print "Route is %d legs totalling %f NM" % (route.leg_count(), route.length_NM())
+    # print "Route is %d legs totalling %f NM" % (route.leg_count(), route.length_NM())
 
     
     # print leg details
 
-    for leg in route.legs:
-        print str(leg)
+    # for leg in route.legs:
+    #     print str(leg)
 
-    v = Vessel("1234", "Trilogy")
+    v = Vessel(503633800, "TRILOGY")
 
     lkp = LKP(v, leg=route.first_leg, pos=route.first_leg.start_point, course=0.0, speed=v.cruise_speed)
-    print(lkp)
+    # print(lkp)
+    # print(lkp.as_AIS_pos_report())
+
+    i = 0
+    while 1:
+        lkp = lkp.next()
+        print(lkp.as_AIS_pos_report())
+        i += 1
+        if i >= 9:
+            print(v.as_AIS_report())
+            i = 0
+        sleep(6)
+         
+
