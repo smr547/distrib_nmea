93f86ae6a0602c19fc643130576e8fd385fc2e27
Steven Ring <smr@southsky.com.au>
2020-05-24 11:25:22 +1000

    New services to run in background on Raspberry PI

---
+++ ruby_princess_LBG.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+./follow_route.py  -m 503633801 -n "RUBY PRINCESS" -s 14.0 -f ./lbg_route.json > ./nmea_fifo
---
+++ trilogy_LBG.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+./follow_route.py  -m 503633800 -n "TRILOGY" -s 6.0 -f ./lbg_route.json > ./nmea_fifo
